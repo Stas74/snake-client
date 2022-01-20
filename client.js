b2871ea748a29392ef292613cded8a14d1cb9456
Stas74 <splotnikov74@gmail.com>
2022-01-19 18:03:05 -0700

    Add messages Connection and Gamer Name

--- client.js
+++ client.js
@@ -13,6 +13,14 @@ const connect =  () => {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on('connect', () => {
+    conn.write("Name: GMT");
+  });
+
   conn.on('data', (data) => {
     console.log(data.toString());
     conn.end();
