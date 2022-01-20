53548e13ccc40c2171466812ee8f18405df1e151
Stas74 <splotnikov74@gmail.com>
2022-01-19 20:38:37 -0700

    Add wasd bind and connection

--- input.js
+++ input.js
@@ -1,13 +1,30 @@
-const handleUserInput = function () {
+let connection;
+
+
+const handleUserInput = function (key) {
   // your code here
   // \u0003 maps to ctrl+c input
-  if ('\u0003') {
+  
+  if (key === '\u0003') {
     process.exit();
+  };
+  if (key === 'w') {
+    connection.write("Move: up");
+  };
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key ==='s') {
+    connection.write("Move: down");
+  };
+  if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.on("data", handleUserInput);
   stdin.setRawMode(true);
@@ -17,3 +34,12 @@ const setupInput = function () {
 };
 
 module.exports = {setupInput};
+
+
+/*
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
+};
+*/
